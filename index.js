d6fc58f4a7434e88e2a25514fd52cc2af3ab5a07
Makemek <makemek101@hotmail.com>
2016-06-10 04:34:37 +0900

    [fix] Core.swf reject to load TitleMain.swf sent from the server
    Because the response header 'Content-Disposition' is set.
    Flash will reject the file immediately and throw Error#2190.
    Just remove that response header out by calling res.sendFile() instead of
    res.download() which set that header.

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ var mime = require('mime');
 
 var app = express();
 
+app.use(function(req, res, next) {
+   res.removeHeader('x-powered-by');
+   next();
+})
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/mainD2.swf', function(req, res) {
    res.sendFile(path.join(__dirname, 'mainD2.swf'))
+})
+
+app.get('/crossdomain.xml', function(req, res) {
+   res.sendFile(path.join(__dirname, 'crossdomain.xml'))
 })
 
 var downloadSWF = function(req, res) {
@@ -24,9 +33,8 @@ var downloadSWF = function(req, res) {
    console.log(filename);
    console.log(mimetype);
 
-   res.setHeader('Content-disposition', 'attachment;filename=' + filename);
    res.setHeader('Content-Type', mimetype);
-   res.download(file)
+   res.sendFile(file)
 }
 
 app.get('/:file', downloadSWF);
