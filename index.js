2f934616f51291a8adf7f227f13755d1eb4a535c
Makemek <makemek101@hotmail.com>
2016-06-10 03:00:19 +0900

    [download file] set response header (Content-disposition, Content-Type)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+var mime = require('mime');
+
 var app = express();
 
 app.get('/', function (req, res) {
@@ -14,11 +16,22 @@ var downloadSWF = function(req, res) {
    console.log('-------Request File-------');
    console.log(req.params);
    console.log(req.query);
-   res.download(path.join(__dirname, req.params.file))
+
+   var file = path.join(__dirname, req.params.file);
+   var filename = path.basename(file);
+   var mimetype = mime.lookup(file);
+
+   console.log(filename);
+   console.log(mimetype);
+
+   res.setHeader('Content-disposition', 'attachment;filename=' + filename);
+   res.setHeader('Content-Type', mimetype);
+   res.download(file)
 }
 
 app.get('/:file', downloadSWF);
 app.get('/scenes/:file', downloadSWF);
+app.get('/resources/swf/:file', downloadSWF);
 
 app.listen(80, function() {
    console.log('Example app listening on port 80');
