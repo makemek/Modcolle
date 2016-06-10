6b6bad928fd8123a1482dfa66f0675c50d3c2996
Makemek <makemek101@hotmail.com>
2016-06-11 01:21:34 +0900

    send local Kancolle's file when Kancolle requests a resource that has url ends with .swf or .mp3

--- routing/loopback.js
+++ routing/loopback.js
@@ -1,3 +1,7 @@
+/*
+ * Handle Kancolle's request to external resources
+ */
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,28 +10,20 @@ var mime = require('mime');
 
 var settings = require('../settings');
 
-// URIs where Kancolle makes a request to external resources
-router.get('/:file', sendStaticFile());
-router.get('/scenes/:file', sendStaticFile());
-router.get('/resources/swf/:file', sendStaticFile());
+var onlyMp3AndSwf = /^.*\.(swf|mp3)$/i;
 
-function sendStaticFile() {
+router.get(onlyMp3AndSwf, function(req, res) {
+   console.log('-------Request File-------');
+   console.log('URL: ' + req.url);
 
-   return function(req, res) {
-      console.log('-------Request File-------');
-      console.log('Filename: ' + req.params.file);
-      console.log('Parameters: ' + JSON.stringify(req.query));
+   var file = path.join(__SERVER_ROOT, settings.KANCOLLE_BASE_DIR, req.baseUrl, req.path);
+   var filename = path.basename(file);
 
-      var file = path.join(__SERVER_ROOT, settings.KANCOLLE_BASE_DIR, req.baseUrl, req.path);
-      var filename = path.basename(file);
+   var mimetype = mime.lookup(file);
+   console.log('Mimetype: ' + mimetype);
 
-      var mimetype = mime.lookup(file);
-      console.log('Mimetype: ' + mimetype);
-
-
-      res.setHeader('Content-Type', mimetype);
-      res.sendFile(file)
-   }
-}
+   res.setHeader('Content-Type', mimetype);
+   res.sendFile(file);
+});
 
 module.exports = exports = router;
