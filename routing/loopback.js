18f47173b6c23f6a90bd81eaa5dece2ce2e81da7
Makemek <makemek101@hotmail.com>
2016-06-12 03:27:05 +0900

    [online resource] include url parameters before sending a request to Kancolle server

--- routing/loopback.js
+++ routing/loopback.js
@@ -16,15 +16,15 @@ router.get('/resources/image/world/:worldImg.png', function(req, res) {
    .replace(/\./g, '_')
    .replace('_png', '.png');
 
-   retrieveLocalKancolleFile(res, worldImageUrl);
+   retrieveLocalKancolleFile(res, worldImageUrl, retriveOnlineKancolleFile(worldImageUrl));
 })
 
-var onlyMp3AndSwf = /^.*\.(swf|mp3|png)$/i;
-router.get(onlyMp3AndSwf, function(req, res) {
-   retrieveLocalKancolleFile(res, path.join(req.baseUrl, req.path));
+var urlEndWithFileType = /^.*\.(swf|mp3|png)$/i;
+router.get(urlEndWithFileType, function(req, res) {
+   retrieveLocalKancolleFile(res, path.join(req.baseUrl, req.path), retriveOnlineKancolleFile(req.originalUrl));
 });
 
-function retrieveLocalKancolleFile(res, path2file) {
+function retrieveLocalKancolleFile(res, path2file, onFileNotFound) {
    console.log('-------Request File-------');
 
    var file = path.resolve(path.join(__SERVER_ROOT, settings.KANCOLLE_BASE_DIR, path2file));
@@ -34,15 +34,17 @@ function retrieveLocalKancolleFile(res, path2file) {
       var fileNotFound = error && error.status === 404;
       if(fileNotFound) {
          console.log(path.basename(file) + ' not found. Retrive from Kancolle server');
-         retriveOnlineKancolleFile(res, path2file);
+         onFileNotFound(res);
       }
    });
 }
 
-function retriveOnlineKancolleFile(res, path2file) {
-   var onlineResourceUrl = kancolleExternal.kcsResource(path2file);
-   console.log('Request external resource ' + onlineResourceUrl);
-   res.redirect(onlineResourceUrl);
+function retriveOnlineKancolleFile(kcsUrl) {
+   return function(res) {
+      var onlineResourceUrl = kancolleExternal.kcsResource(kcsUrl);
+      console.log('Request external resource ' + onlineResourceUrl);
+      res.redirect(onlineResourceUrl);
+   }
 }
 
 module.exports = exports = router;
