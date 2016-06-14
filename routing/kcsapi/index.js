826fc46017bb76a42078e9d40c447845623faaf4
Makemek <makemek101@hotmail.com>
2016-06-14 19:33:32 +0900

    [optimization] decode API response asynchronously
    Now nodeJs thread don't have to wait for synchronous operation to finish

--- routing/kcsapi/index.js
+++ routing/kcsapi/index.js
@@ -15,29 +15,34 @@ router.post('/*', function(req, res, next) {
 
 	agent.apiRequest(apiUrl, req, function(error, httpResponse, body) {
 		if(error)
-			next(error);
+			return next(error);
 
-		var response = decodeResponse(httpResponse, body);
-		// uncomment this to view response from API
-		console.log('----API RESPONSE----');
-		console.log(response); 
-		console.log('----END RESPONSE----');
-		res.send(response);
+		decodeResponse(httpResponse, body, function(response) {
+			// uncomment this to view response from API
+			console.log('----API RESPONSE----');
+			console.log(response); 
+			console.log('----END RESPONSE----');
+			res.send(response);
+		});
 	});
 	// res.redirect(307, apiUrl);
 
-	function decodeResponse(httpResponse, buffer) {
-		var stringEncodeMethod = 'utf-8'
-
-		switch (httpResponse.headers['content-encoding']) {
-			case 'gzip':
-			case 'deflate':
-				return zlib.unzipSync(buffer).toString(stringEncodeMethod); break;
-
-			default:
-				return buffer.toString(stringEncodeMethod);
-		}
-	}
 })
+
+function decodeResponse(httpResponse, buffer, onDone) {
+	var stringEncodeMethod = 'utf-8';
+
+	switch (httpResponse.headers['content-encoding']) {
+		case 'gzip':
+		case 'deflate':
+			zlib.unzip(buffer, function(error, bufferDecompress) {
+				onDone(bufferDecompress.toString(stringEncodeMethod));
+			}); 
+			break;
+
+		default:
+			onDone(buffer.toString(stringEncodeMethod));
+	}
+}
 
 module.exports = exports = router;
