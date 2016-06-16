627b65ee9bbb4e9bd64bc45034e5f807d692a4e8
Makemek <makemek101@hotmail.com>
2016-06-17 01:24:34 +0700

    [security] do NOT send 'api_token' and 'api_starttime' in URL paramters
    file mainD2.swf sends out api_token and api_starttime to Kancolle servers
    in GET URL parameters (mainD2.swf?api_token=...&api_starttime=...)
    ModColle will remove these two parameters from the URL.
    Although Kancolle can detect that ModColle is running, it doesn't know who
    run it

--- scripts/model/agent.js
+++ scripts/model/agent.js
@@ -4,6 +4,7 @@ const settings = require('nconf');
 const request = require('request');
 const path = require('path');
 const urljoin = require('url-join');
+const urlparse = require('url-parse');
 const appLog = require('winston').loggers.get('app');
 
 const kancolleExternal = require('./kancolleExternal');
@@ -17,7 +18,10 @@ var agent = {
 	},
 
 	download: function(res, url, onResponse) {
-		return request.get(url).on('response', onResponse).pipe(res);
+		var parsedUrl = removeUrlParameterSensitiveData(url);
+		appLog.verbose('Remove sensitive data in URL parameters');
+		appLog.verbose('Parsed URL: ' + parsedUrl);
+		return request.get(parsedUrl).on('response', onResponse).pipe(res);
 	},
 
 	apiRequest: function(_url, req, onResponse) {
@@ -54,6 +58,14 @@ function forgeKancolleHttpRequestHeader(httpHeader) {
 		headers['referer'] = httpHeader.referer.replace(httpHeader.host, serverIp);
 		headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36';
 	}
+}
+
+function removeUrlParameterSensitiveData(url) {
+	url = urlparse(url, true);
+	delete url.query.api_token;
+	delete url.query.api_starttime;
+
+	return url.toString();
 }
 
 module.exports = exports = agent;
