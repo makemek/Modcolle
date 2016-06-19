0a93f7659e8056bcad2e69137950aaa79007322f
Makemek <makemek101@hotmail.com>
2016-06-19 17:28:02 +0700

    [fix] request world image fail when world server ip does not have leading zeros eg. 123.001.001.010
    When access the server with ip address eg.
    127.0.0.1 -> Kancolle will request image name 127_000_000_001_t.png
    localhost -> Kancolle will request image name localhost_t.png
    
    Modcolle will convert world ip address fill with leading zeros to get the
    image

--- scripts/routing/loopback.js
+++ scripts/routing/loopback.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const validator = require('validator');
 
 var settings = require('nconf');
 var kancolleExternal = require('../model/kancolleExternal');
@@ -14,10 +15,15 @@ const expressLog = require('winston').loggers.get('express');
 
 router.get('/resources/image/world/:worldImg.png', function(req, res, next) {
    expressLog.info('GET: ' + req.originalUrl);
-   var host = new RegExp(req.headers.host, 'gi');
-   var worldImageUrl = req.url.replace(host, settings.get('MY_WORLD_SERVER'))
-   .replace(/\./g, '_')
-   .replace('_png', '.png');
+
+   var imageName = req.params.worldImg.replace('_t', '');
+   var worldServerImageName = convertToWorldImageFilename(imageName, req.headers.host, settings.get('MY_WORLD_SERVER'));
+   var worldImageUrl = req.url.replace(imageName, worldServerImageName);
+
+   appLog.debug('Image name: ' + req.params.worldImg);
+   appLog.debug('Host name: ' + req.headers.host);
+   appLog.debug('World image filename: ' + worldServerImageName);
+   appLog.debug('World image URL: ' + worldImageUrl);
 
    agent.load(res, worldImageUrl, handleFileNotFound(worldImageUrl, res, next));
 })
@@ -53,6 +59,25 @@ function handleFileNotFound(urlDownload, res, next) {
          return next(error);
       }
    }
+}
+
+function convertToWorldImageFilename(imageName, thisHostName, worldServerIp) {
+   var isHostName = imageName == thisHostName;
+   var isIpAddress = validator.isIP(imageName.split('_').map(Number).join('.'));
+   var worldServerImageName;
+   if(isHostName || isIpAddress) {
+      var ip = worldServerIp.split('.');
+      for(var n = 0; n < ip.length; ++n)
+         ip[n] = ("000" + ip[n]).substr(-3,3);
+
+      worldServerImageName = ip.join('_');
+   }
+   else {
+      appLog.warn(imageName + ' is neither a host name nor ip address');
+      worldServerImageName = imageName;
+   }
+
+   return worldServerImageName;
 }
 
 module.exports = exports = router;
