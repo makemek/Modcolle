3df71ad9e7ad82ee4c4810399d831663432d3399
Makemek <makemek101@hotmail.com>
2016-06-20 20:27:58 +0700

    logging worker initialization

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,26 @@
 
 const throng = require('throng');
 const server = require('./server');
+const winston = require('winston');
 
-function forkWorker() {
-	throng(function(workerId) {
-		server(80);
+var multiCoreInit = throng({
+	master: startMaster,
+	start: startWorker
+})
+
+function startMaster() {
+	winston.info('Initialize multi-core');
+}
+
+function startWorker(id) {
+
+	winston.info(`Start worker id ${id}`);
+	server(80);
+
+	process.on('SIGTERM', function() {
+		winston.info(`Terminate worker id ${id}`);
+		process.exit();
 	})
 }
 
-module.exports = exports = forkWorker();
+module.exports = exports = multiCoreInit;
