0007eb5d82803c16f4170b95b9b66c252d6d2f66
Makemek <makemek101@hotmail.com>
2016-06-20 19:19:19 +0700

    initialize multi-core cpu using throng

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,12 @@
 'use strict';
 
-const nconf = require('nconf');
-const winston = require('winston');
-loadConfig();
+const throng = require('throng');
+const server = require('./server');
 
-const App = require('./modcolle');
-
-function startServer(port) {
-	var modColle = new App(port);
-	modColle.start(function() {
-		winston.loggers.get('app').info('ModColle is ready!');
+function forkWorker() {
+	throng(function(workerId) {
+		server(80);
 	})
 }
 
-function loadConfig() {
-	nconf.argv().env();
-	nconf.add('settings', {type: 'file', file: 'scripts/config/settings.json'});
-	nconf.add('logger', {type: 'file', file: 'scripts/config/loggerSettings.json'});
-
-	setupLogger();
-}
-
-function setupLogger() {
-	var loggerConfig = nconf.get('logger');
-	Object.keys(loggerConfig).forEach(function(key) {
-	    winston.loggers.add(key, loggerConfig[key]);
-	});
-}
-
-module.exports = exports = startServer(80);
+module.exports = exports = forkWorker();
---
+++ scripts/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const nconf = require('nconf');
+const winston = require('winston');
+loadConfig();
+
+const App = require('./modcolle');
+
+function startServer(port) {
+	var modColle = new App(port);
+	modColle.start(function() {
+		winston.loggers.get('app').info('ModColle is ready!');
+	})
+}
+
+function loadConfig() {
+	nconf.argv().env();
+	nconf.add('settings', {type: 'file', file: 'scripts/config/settings.json'});
+	nconf.add('logger', {type: 'file', file: 'scripts/config/loggerSettings.json'});
+
+	setupLogger();
+}
+
+function setupLogger() {
+	var loggerConfig = nconf.get('logger');
+	Object.keys(loggerConfig).forEach(function(key) {
+	    winston.loggers.add(key, loggerConfig[key]);
+	});
+}
+
+module.exports = exports = startServer;
