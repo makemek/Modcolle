4330b8065927459262135be02dcc9fda4cd8a477
Makemek <makemek101@hotmail.com>
2016-06-25 03:28:18 +0700

    testcase: load file locally

--- test/spec/test-agent.js
+++ test/spec/test-agent.js
@@ -1,16 +1,32 @@
 'use strict';
 
 const agent = require('../../scripts/model/agent');
+const sinon = require('sinon');
+const nconf = require('nconf');
 
 describe('kancolle agent', function() {
 
-	it('load non-existant file', function() {
+	it('load file locally', sinon.test(function() {
+		const FILE = 'someFile';
+		const BASE_DIR = 'base';
 
-	})
+		var configMock = sinon.mock(nconf);
+		configMock.expects('get').withArgs('KANCOLLE_BASE_DIR').returns(BASE_DIR);
 
-	it('load existing file', function() {
+		var res = {
+			sendFile: function(file, arg, onError){}
+		}
+		var sendFile = sinon.stub(res, 'sendFile');
+		var errorCallback = sinon.stub();
 
-	})
+		agent.load(res, 'someFile', errorCallback);
+		sinon.assert.calledOnce(sendFile);
+
+		var args = sendFile.firstCall.args;
+		assert.include(args, errorCallback, 'error callback not include');
+		assert.include(args[0], BASE_DIR);
+		assert.include(args[0], FILE);
+	}))
 
 	it('download with valid url', function() {
 
