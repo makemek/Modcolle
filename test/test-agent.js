2e671ed7e2492657d22e65fd2d140ae0d56ac2b3
Makemek <makemek101@hotmail.com>
2016-06-25 01:16:15 +0700

    setup test with mocha

---
+++ test/test-agent.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const agent = require('../scripts/model/agent');
+
+describe('kancolle agent', function() {
+
+	it('load non-existant file', function() {
+
+	})
+
+	it('load existing file', function() {
+
+	})
+
+	it('download with valid url', function() {
+
+	})
+
+	it('download with invalid url', function() {
+
+	})
+
+	it('call api with valid url', function() {
+
+	})
+
+	it('call api with invalid url', function() {
+		
+	})
+})
