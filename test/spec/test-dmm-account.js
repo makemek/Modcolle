72bfe1d1046ceb5d5beee87eeef72fc53ce4fbbd
Makemek <makemek101@hotmail.com>
2016-06-25 16:23:39 +0700

    define testcases

---
+++ test/spec/test-dmm-account.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const DmmAccount = require('../../scripts/model/dmmAccount');
+const sinon = require('sinon');
+
+describe('DMM account', function() {
+
+	it.skip('empty email', function() {
+		
+	})
+
+	it.skip('empty password', function() {
+
+	})
+
+	it.skip('login fail due to incorrect email or password', function() {
+
+	})
+
+	it.skip('login success', function() {
+
+	})
+
+})
