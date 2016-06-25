1188e0c05b4b894aea3ffae23ce5cd9494c22b12
Makemek <makemek101@hotmail.com>
2016-06-25 17:17:58 +0700

    wrap testcases in sinon.test

--- test/spec/test-dmm-account.js
+++ test/spec/test-dmm-account.js
@@ -5,21 +5,21 @@ const sinon = require('sinon');
 
 describe('DMM account', function() {
 
-	it('empty email', function() {
-		var onFailureCallback = sinon.spy();
+	it('empty email', sinon.test(function() {
+		var onFailureCallback = this.spy();
 		var account = new DmmAccount('', '1234');
 		account.login(function(){}, onFailureCallback);
 
 		sinon.assert.called(onFailureCallback);
-	})
+	}))
 
-	it('empty password', function() {
-		var onFailureCallback = sinon.spy();
+	it('empty password', sinon.test(function() {
+		var onFailureCallback = this.spy();
 		var account = new DmmAccount('john@example.com', '');
 		account.login(function(){}, onFailureCallback);
 
 		sinon.assert.called(onFailureCallback);
-	})
+	}))
 
 	it.skip('login fail due to incorrect email or password', function() {
 
