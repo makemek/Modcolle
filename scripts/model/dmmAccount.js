23905e4b3f6f9018247ab85b1f626ab22cf536cb
Makemek <makemek101@hotmail.com>
2016-06-25 16:34:05 +0700

    [DmmAccount] handle failure on empty email

--- scripts/model/dmmAccount.js
+++ scripts/model/dmmAccount.js
@@ -4,12 +4,13 @@ const inherit = require('inherit');
 
 var DmmAccount = {
 	__constructor: function(email, password) {
-		this.email = email;
-		this.password = password;
+		this.email = email || '';
+		this.password = password || '';
 	},
 
 	login: function(onSuccess, onFailure) {
-
+		if(!this.email.length)
+			return onFailure('email is empty');
 	}
 }
 
--- test/spec/test-dmm-account.js
+++ test/spec/test-dmm-account.js
@@ -5,8 +5,12 @@ const sinon = require('sinon');
 
 describe('DMM account', function() {
 
-	it.skip('empty email', function() {
-		
+	it('empty email', function() {
+		var onFailureCallback = sinon.spy();
+		var account = new DmmAccount('', '1234');
+		account.login(function(){}, onFailureCallback);
+
+		sinon.assert.called(onFailureCallback);
 	})
 
 	it.skip('empty password', function() {
