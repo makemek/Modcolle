394f3450c243aa3549e59f881f13c24f829eee4c
Makemek <makemek101@hotmail.com>
2016-06-25 14:22:15 +0700

    testcase: download from network

--- test/spec/test-agent.js
+++ test/spec/test-agent.js
@@ -3,15 +3,29 @@
 const agent = require('../../scripts/model/agent');
 const sinon = require('sinon');
 const nconf = require('nconf');
+const request = require('request');
+const kancolleExternal = require('../../scripts/model/kancolleExternal');
 
 describe('kancolle agent', function() {
 
+	var configMock;
+	const KANCOLLE_CONFIG = {
+		baseDir: 'base',
+		serv: '0.0.0.0'
+	}
+
+	beforeEach(function() {
+		configMock = sinon.mock(nconf);
+	})
+
+	afterEach(function() {
+		configMock.restore();
+	})
+
 	it('load file locally', sinon.test(function() {
 		const FILE = 'someFile';
-		const BASE_DIR = 'base';
 
-		var configMock = sinon.mock(nconf);
-		configMock.expects('get').withArgs('KANCOLLE_BASE_DIR').returns(BASE_DIR);
+		configMock.expects('get').withArgs('KANCOLLE_BASE_DIR').returns(KANCOLLE_CONFIG.baseDir);
 
 		var res = {
 			sendFile: function(file, arg, onError){}
@@ -24,17 +38,39 @@ describe('kancolle agent', function() {
 
 		var args = sendFile.firstCall.args;
 		assert.include(args, errorCallback, 'error callback not include');
-		assert.include(args[0], BASE_DIR);
+		assert.include(args[0], KANCOLLE_CONFIG.baseDir);
 		assert.include(args[0], FILE);
 	}))
 
-	it('download with valid url', function() {
+	it('download from network', sinon.test(function() {
+		var sensitiveParams = '?api_token=abc&api_starttime=1234'
+		var url = 'http://www.example.com';
+		const KCS_URL = 'http://www.kcs.com';
 
-	})
+		configMock.expects('get').withArgs('MY_WORLD_SERVER').returns(KANCOLLE_CONFIG.serv);
 
-	it('download with invalid url', function() {
+		var res = sinon.spy();
+		var callback = sinon.spy();
+		var mockRes = {
+			on: function() {
+				return this;
+			},
 
-	})
+			pipe: function(_res) {
+				return this;
+			}
+		}
+		var httpGet = sinon.stub(request, 'get').returns(mockRes);
+		sinon.stub(kancolleExternal, 'host').returns(KCS_URL);
+
+		agent.download(res, url + sensitiveParams, callback);
+
+		sinon.assert.calledOnce(httpGet);
+		var param = httpGet.firstCall.args[0];
+		expect(param.url).to.equal(url);
+		expect(param.headers.host).to.equal(KANCOLLE_CONFIG.serv);
+		expect(param.headers['x-requested-with']).to.match(/flash/i);
+	}))
 
 	it('call api with valid url', function() {
 
