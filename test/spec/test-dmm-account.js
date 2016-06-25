89b7cdd089f8e2bd4f563558fa49f65a325387bd
Makemek <makemek101@hotmail.com>
2016-06-25 17:24:44 +0700

    pass parameter to login() with cookieCallback which contains error and result
    refactor testcases

--- test/spec/test-dmm-account.js
+++ test/spec/test-dmm-account.js
@@ -6,27 +6,40 @@ const sinon = require('sinon');
 describe('DMM account', function() {
 
 	it('empty email', sinon.test(function() {
-		var onFailureCallback = this.spy();
 		var account = new DmmAccount('', '1234');
-		account.login(function(){}, onFailureCallback);
-
-		sinon.assert.called(onFailureCallback);
+		expectError(account);
 	}))
 
 	it('empty password', sinon.test(function() {
-		var onFailureCallback = this.spy();
 		var account = new DmmAccount('john@example.com', '');
-		account.login(function(){}, onFailureCallback);
-
-		sinon.assert.called(onFailureCallback);
+		expectError(account);
 	}))
 
-	it.skip('login fail due to incorrect email or password', function() {
+	it.skip('login fail due to incorrect email or password', sinon.test(function() {
+		var cookieCallback = this.spy();
+		var account = new DmmAccount('john@example.com', '1234');
+		account.login(cookieCallback);
 
-	})
+		sinon.assert.called(cookieCallback);
+		console.log(cookieCallback.args);
+	}))
 
 	it.skip('login success', function() {
 
 	})
 
 })
+
+function expectError(account) {
+	var cookieCallback = sinon.spy();
+
+	account.login(cookieCallback);
+
+	sinon.assert.calledOnce(cookieCallback);
+	var error = cookieCallback.args[0][0];
+	expect(error).to.be.an('error');
+
+	sinon.restore();
+
+	return error;
+}
