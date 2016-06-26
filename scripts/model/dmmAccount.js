d2a229883f83a8170ca2fe53b0764a18aff406b1
Makemek <makemek101@hotmail.com>
2016-06-26 13:51:55 +0700

    scrape login token from DMM

--- scripts/model/dmmAccount.js
+++ scripts/model/dmmAccount.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const inherit = require('inherit');
+const rp = require('request-promise');
+const async = require('async');
 
 var DmmAccount = {
 	__constructor: function(email, password) {
@@ -11,6 +13,23 @@ var DmmAccount = {
 	login: function(cookieCallback) {
 		if(!this.email.length) return cookieCallback(new Error('email is empty'));
 		if(!this.password.length) return cookieCallback(new Error('password is empty'));
+
+		async.waterfall([
+			scrapeToken()
+		], cookieCallback);
+	}
+}
+
+function scrapeToken() {
+	return function(done) {
+		rp.get({
+			uri: 'https://www.dmm.com/my/-/login/=/path=Sg__/',
+		}).then(function(htmlBody) {
+			var tokens = htmlBody.match(/[a-f0-9]{32}/g);
+			const DMM_TOKEN = tokens[1];
+			const DATA_TOKEN = tokens[2];
+			return done(null, DMM_TOKEN, DATA_TOKEN);	
+		}).catch(done);
 	}
 }
 
--- test/spec/test-dmm-account.js
+++ test/spec/test-dmm-account.js
@@ -2,6 +2,8 @@
 
 const DmmAccount = require('../../scripts/model/dmmAccount');
 const sinon = require('sinon');
+const rp = require('request-promise');
+const sprintf = require("sprintf-js").sprintf
 
 describe('DMM account', function() {
 
@@ -15,13 +17,15 @@ describe('DMM account', function() {
 		expectError(account);
 	}))
 
-	it.skip('login fail due to incorrect email or password', sinon.test(function() {
-		var cookieCallback = this.spy();
+	it.only('login fail due to incorrect email or password', sinon.test(function() {
 		var account = new DmmAccount('john@example.com', '1234');
-		account.login(cookieCallback);
 
-		sinon.assert.called(cookieCallback);
-		console.log(cookieCallback.args);
+		var loginToken = srapeLoginPageTokenMock();
+		this.stub(rp, 'get').onFirstCall().returns(loginToken.response);
+
+		account.login(function(err, cookie) {
+			done(err);
+		});
 	}))
 
 	it.skip('login success', function() {
@@ -42,4 +46,42 @@ function expectError(account) {
 	sinon.restore();
 
 	return error;
+}
+
+function srapeLoginPageTokenMock() {
+	var fakeToken = {
+		formToken: '0123456789abcdef0000000000000000',
+		dmmToken: '0123456789abcdef1111111111111111',
+		dataToken: '0123456789abcdef2222222222222222'
+	}
+	
+	var data = {
+		fakeToken: fakeToken,
+		fakeHtml: getFakeHtml(),
+		response: {
+			then: function(htmlBodyCallback) {
+				var body = new String(getFakeHtml());
+				var spy = sinon.spy(body, 'match');
+				htmlBodyCallback(body);
+				
+				var returnVal = spy.firstCall.returnValue;
+				assert.include(returnVal, fakeToken.dmmToken);
+				assert.include(returnVal, fakeToken.dataToken);
+
+				spy.restore();
+				return this;
+			},
+			catch: function(error) {return this;}
+		}
+	}
+
+	function getFakeHtml() {
+		var htmlForm = sprintf('<input type="hidden" name="token" value="%s" id="id_token">', fakeToken.formToken);
+		var ajaxDmmToken = sprintf('xhr.setRequestHeader("DMM_TOKEN", "%s");', fakeToken.dmmToken);
+		var ajaxDataToken = sprintf('"token": "%s"', fakeToken.dataToken);
+
+		return htmlForm + ajaxDmmToken + ajaxDataToken;
+	}
+
+	return data;
 }
