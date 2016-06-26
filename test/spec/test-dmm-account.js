cc3ce2a1f4031949a757486108a1492e20d66c7b
Makemek <makemek101@hotmail.com>
2016-06-26 15:56:02 +0700

    test scrape login token as individual task

--- test/spec/test-dmm-account.js
+++ test/spec/test-dmm-account.js
@@ -4,6 +4,7 @@ const DmmAccount = require('../../scripts/model/dmmAccount');
 const sinon = require('sinon');
 const rp = require('request-promise');
 const sprintf = require("sprintf-js").sprintf
+const async = require('async');
 
 describe('DMM account', function() {
 
@@ -17,11 +18,34 @@ describe('DMM account', function() {
 		expectError(account);
 	}))
 
-	it.only('login fail due to incorrect email or password', sinon.test(function() {
+	it('scrape login token from DMM login page', sinon.test(function() {
+		var procedure = this.stub(async, 'waterfall');
+		
+		var account = new DmmAccount('john@example.com', '1234');
+		account.login();
+		
+		var task = procedure.firstCall.args[0][0];
+		var loginToken = mockSrapeLoginPageToken();
+		var httpRequest = this.stub(rp, 'get').returns(loginToken.response);
+
+		var spyDone = this.spy();
+		task(spyDone);
+		assert.isTrue(spyDone.calledOnce);
+
+		var returnVal = spyDone.firstCall.args;
+		assert.isNull(returnVal[0]);
+		assert.equal(returnVal[1], loginToken.fakeToken.dmmToken);
+		assert.equal(returnVal[2], loginToken.fakeToken.dataToken);
+
+		var httpParam = httpRequest.firstCall.args[0];
+		assert.match(httpParam.uri, new RegExp('https://www.dmm.com/my/-/login/*'));
+	}))
+
+	it.skip('login fail due to incorrect email or password', sinon.test(function() {
 		var account = new DmmAccount('john@example.com', '1234');
 
-		var loginToken = srapeLoginPageTokenMock();
-		this.stub(rp, 'get').onFirstCall().returns(loginToken.response);
+		var authorizeToken = mockAuthorizeDmmToken();
+		var ajaxRequest = this.stub(rp, 'post').onFirstCall().returns(authorizeToken.response);
 
 		account.login(function(err, cookie) {
 			done(err);
@@ -48,7 +72,7 @@ function expectError(account) {
 	return error;
 }
 
-function srapeLoginPageTokenMock() {
+function mockSrapeLoginPageToken() {
 	var fakeToken = {
 		formToken: '0123456789abcdef0000000000000000',
 		dmmToken: '0123456789abcdef1111111111111111',
@@ -60,15 +84,7 @@ function srapeLoginPageTokenMock() {
 		fakeHtml: getFakeHtml(),
 		response: {
 			then: function(htmlBodyCallback) {
-				var body = new String(getFakeHtml());
-				var spy = sinon.spy(body, 'match');
-				htmlBodyCallback(body);
-				
-				var returnVal = spy.firstCall.returnValue;
-				assert.include(returnVal, fakeToken.dmmToken);
-				assert.include(returnVal, fakeToken.dataToken);
-
-				spy.restore();
+				htmlBodyCallback(getFakeHtml());
 				return this;
 			},
 			catch: function(error) {return this;}
@@ -86,7 +102,7 @@ function srapeLoginPageTokenMock() {
 	return data;
 }
 
-function ajaxAuthorizeDmmTokenMock() {
+function mockAuthorizeDmmToken() {
 	var fakeToken = {};
 	fakeToken['token'] = '0123456789abcdef3333333333333333';
 	fakeToken['login_id'] = '0123456789abcdef4444444444444444';
