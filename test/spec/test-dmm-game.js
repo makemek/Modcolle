84d6ee26ef46165265ab6df967d82120fe712c69
Makemek <makemek101@hotmail.com>
2016-07-03 23:12:42 +0700

    parameterized test of game id

--- test/spec/test-dmm-game.js
+++ test/spec/test-dmm-game.js
@@ -10,12 +10,25 @@ describe.only('DMM netgame', function() {
 		var game = new DmmGame(account);
 	})
 
+	var netGame = gameIdList();
+	for(var game in netGame) {
+		it('should contain id of game ' + game, createTestParameterizeGameList(game, netGame[game]))
+	}
+
 	it('should get DMM gadget information about the game', function() {
 
 	})
-
-	it('should contain correct game id', function() {
-
-	})
-
 })
+
+function gameIdList() {
+	return {
+		kancolle: 854854
+	}
+}
+
+function createTestParameterizeGameList(gameName, expectedId) {
+	return function() {
+		assert.isObject(DmmGame.id, 'DmmGame should have property id');
+		assert.equal(DmmGame.id[gameName], expectedId, 'game id should match');	
+	}
+}
