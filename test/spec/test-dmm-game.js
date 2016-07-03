ca450e1a84734f36ea2e5ecbcd7dd03e497e508e
Makemek <makemek101@hotmail.com>
2016-07-03 22:25:33 +0700

    test-dmm-game.js with testcases

---
+++ test/spec/test-dmm-game.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const dmmGame = require('../../scripts/model/dmmGame');
+
+describe('DMM netgame', function() {
+
+	it('should get DMM gadget information about the game', function() {
+
+	})
+
+	it('should contain correct game id', function() {
+
+	})
+
+})
