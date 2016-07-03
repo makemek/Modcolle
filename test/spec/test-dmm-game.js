820730f591379748ebb57ec25e7a6848082446cb
Makemek <makemek101@hotmail.com>
2016-07-04 00:13:17 +0700

    [test dmm-game] implement testcase gadget info

--- test/spec/test-dmm-game.js
+++ test/spec/test-dmm-game.js
@@ -2,8 +2,11 @@
 
 const DmmGame = require('../../scripts/model/dmmGame');
 const Account = require('../../scripts/model/dmmAccount');
+const sinon = require('sinon');
+const async = require('async');
+const request = require('request');
 
-describe('DMM game abstract class', function() {
+describe.only('DMM game abstract class', function() {
 
 	var dmmGame, account;
 
@@ -12,8 +15,20 @@ describe('DMM game abstract class', function() {
 		dmmGame = new DmmGame(account);
 	})
 
-	it('get expected gadget information', function() {
-		
-	})
+	it('get expected gadget information', sinon.test(function() {
+		var fakeCookie = 'a=1; b=2;';
+		var fakeAppId = -1;
+		var spyPreload = this.spy(dmmGame, '_preload');
+		var stubCookie = this.stub(account, 'getCookie').returns(fakeCookie);
+		var stubAppId = this.stub(dmmGame, '_getAppId').returns(fakeAppId);
+		var procedure = this.stub(async, 'waterfall');
+
+		dmmGame.start();
+
+		var taskAppInfo = procedure.firstCall.args[0];
+		var taskPreload = procedure.firstCall.args[1];
+
+		assert.deepEqual(taskPreload, dmmGame._preload);
+	}))
 
 })
