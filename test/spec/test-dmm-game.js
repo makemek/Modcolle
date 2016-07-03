86d20755f0952d4a048217621bee1befb21015e7
Makemek <makemek101@hotmail.com>
2016-07-03 22:30:33 +0700

    setup before each and describe.only

--- test/spec/test-dmm-game.js
+++ test/spec/test-dmm-game.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const dmmGame = require('../../scripts/model/dmmGame');
+const DmmGame = require('../../scripts/model/dmmGame');
+const Account = require('../../scripts/model/dmmAccount');
 
-describe('DMM netgame', function() {
+describe.only('DMM netgame', function() {
+
+	beforeEach(function() {
+		var account = new Account('poi@poi.com', 'poipoi');
+		var game = new DmmGame(account);
+	})
 
 	it('should get DMM gadget information about the game', function() {
 
