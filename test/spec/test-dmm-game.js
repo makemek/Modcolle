31b10ecedb9d6307e79acc368638526b8e344087
Makemek <makemek101@hotmail.com>
2016-07-11 02:48:43 +0700

    [test dmmGame] test without mocking async module because this testcase tests too specific

--- test/spec/test-dmm-game.js
+++ test/spec/test-dmm-game.js
@@ -15,47 +15,34 @@ describe('DMM game abstract class', function() {
 		dmmGame = new DmmGame(account);
 	})
 
-	it('get expected gadget information', sinon.test(function() {
+	it('get expected gadget information', sinon.test(function(done) {
 		var fakeCookie = 'ccky=1; a=1; b=2;';
 		var fakeAppId = -1;
 		var stubCookie = this.stub(account, 'getCookie').returns(fakeCookie);
 		var stubAppId = this.stub(dmmGame, '_getAppId').returns(fakeAppId);
-		var procedure = this.stub(async, 'waterfall');
-
-		dmmGame.start();
-
-		assert.isTrue(procedure.called);
-		var task = procedure.firstCall.args[0];
-		var taskAppInfo = task[0];
-		var spyDone = this.spy();
+		var stubPreload = this.stub(dmmGame, '_preload', function() {done()});
 		var httpRequest = this.stub(rp, 'get').returns(getFakeResponse());
 
-		taskAppInfo(spyDone);
+		dmmGame.start(function(gadgetInfo) {
+			var rpParam = httpRequest.firstCall.args[0];
+			assert.equal(rpParam.uri, 'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=' + fakeAppId, 'http url should match');
+			assert.equal(rpParam.headers.cookie, fakeCookie, 'cookie should not be altered');
 
-		assert.isTrue(stubCookie.called);
-		assert.isTrue(stubAppId.called);
+			var expectedParsedGadget = {
+			    VIEWER_ID : 123,
+			    OWNER_ID  : 123,
+			    APP_ID    : 456,
+			    URL       : "http://www.example.com",
+			    FRAME_ID  : "game_frame",
+			    ST        : "0123456789abcdefghijklmnopqrstuvwxyz",
+			    TIME      : 1467570034,
+			    TYPE      : "",
+			    SV_CD     : "xx_xxxxxx"
+			};
 
-		var rpParam = httpRequest.firstCall.args[0];
-		assert.isTrue(spyDone.called, 'http GET should be called');
-		assert.equal(rpParam.uri, 'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=' + fakeAppId, 'http url should match');
-		assert.equal(rpParam.headers.cookie, fakeCookie, 'cookie should not be altered');
-
-		var doneArgs = spyDone.firstCall.args;
-		assert.isNull(doneArgs[0], 'there should be no error');
-		var gadgetInfo = doneArgs[1];
-		var expectedParsedGadget = {
-		    VIEWER_ID : 123,
-		    OWNER_ID  : 123,
-		    APP_ID    : 456,
-		    URL       : "http://www.example.com",
-		    FRAME_ID  : "game_frame",
-		    ST        : "0123456789abcdefghijklmnopqrstuvwxyz",
-		    TIME      : 1467570034,
-		    TYPE      : "",
-		    SV_CD     : "xx_xxxxxx"
-		};
-
-		assert.deepEqual(gadgetInfo, expectedParsedGadget, 'gadgetInfo should have the same expected properties and values');
+			assert.deepEqual(gadgetInfo, expectedParsedGadget, 'gadgetInfo should have the same expected properties and values');	
+			done();
+		});
 	}))
 })
 
