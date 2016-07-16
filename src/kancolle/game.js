c92294239891781215f032345ec0351b5307fbff
Makemek <makemek101@hotmail.com>
2016-07-16 21:46:48 +0700

    get maintenance info from http://203.104.209.7/gadget/js/kcs_const.js

--- src/kancolle/game.js
+++ src/kancolle/game.js
@@ -2,13 +2,40 @@
 
 const inherit = require('inherit');
 const DmmGameAbstrct = require('../dmm/game');
+const rp = require('request-promise');
 
 var Kancolle = {
 
 	_getAppId: function() {
 		return 854854;
-	}
+	},
 
+	isOnMaintenance(done) {
+		var options = {
+			url: 'http://203.104.209.7/gadget/js/kcs_const.js'
+		}
+		rp.get(options).then(function(body) {
+			var infoPlainText = body.match(/MaintenanceInfo\..*/g);
+			var infoJson = asJson(infoPlainText, 'MaintenanceInfo');
+			done(null, infoJson)
+		}).catch(done)
+	}
+}
+
+function asJson(setterAssignmentLines, setterVarName) {
+	var infoJsonPlain = '{' + 
+			setterAssignmentLines.replace(setterVarName + '.', '')
+			.replace(';', '')
+			.replace('=', ':') +
+		'}';
+
+	varList = setterAssignmentLines.match(/\w+ /g);
+	varList.forEach(function(property) {
+		property = property.trim();
+		setterAssignmentLines.replace(property, '"' + property + '"');
+	})
+
+	return JSON.parse(setterAssignmentLines);
 }
 
 module.exports = exports = inherit(DmmGameAbstrct, Kancolle);
--- test/spec/test-kancolle-game.js
+++ test/spec/test-kancolle-game.js
@@ -2,7 +2,8 @@
 
 const Account = require('../../src/dmm/account');
 const Kancolle = require('../../src/kancolle/game');
-
+const rp = require('request-promise');
+const sinon = require('sinon');
 
 describe('Kancolle game', function() {
 
@@ -18,4 +19,28 @@ describe('Kancolle game', function() {
 		assert.equal(kancolle._getAppId(), ID, 'Kancolle app id should be ' + ID);
 	})
 
+	it('is NOT on maintenance', function(done) {
+		var sourceText = 
+		`
+		var ConstServerInfo = {}, ConstURLInfo = {};
+		var MaintenanceInfo = {};
+		MaintenanceInfo.IsDoing       = 0;
+		MaintenanceInfo.IsEmergency   = 0;
+		MaintenanceInfo.StartDateTime = Date.parse("2016/07/15 11:00:00");
+		MaintenanceInfo.EndDateTime   = Date.parse("2016/07/15 17:50:00");
+		`
+
+		var httpRequest = sinon.stub(rp, 'get').returns({
+			then: function(bodyCallback) {bodyCallback(sourceText); return this;},
+			catch: function() {}
+		})
+
+		kancolle.isOnMaintenance(function(error, isMaintain) {
+			assert.isNull(error);
+			assert.isFalse(isMaintain);
+
+			httpRequest.restore();
+			done();
+		})
+	})
 })
