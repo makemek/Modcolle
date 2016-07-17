79fd77a6e89d506f2b151de13257d528d69720d1
Makemek <makemek101@hotmail.com>
2016-07-17 18:25:35 +0700

    [test] Kancolle maintenance mode

--- test/spec/test-kancolle-game.js
+++ test/spec/test-kancolle-game.js
@@ -4,6 +4,8 @@ const Account = require('../../src/dmm/account');
 const Kancolle = require('../../src/kancolle/game');
 const rp = require('request-promise');
 const sinon = require('sinon');
+const async = require('async');
+const sprintf = require('sprintf-js').sprintf;
 
 describe('Kancolle game', function() {
 
@@ -12,6 +14,7 @@ describe('Kancolle game', function() {
 	beforeEach(function() {
 		var account = new Account('poi@poi.com', 'poipoi');
 		kancolle = new Kancolle(account);
+
 	})
 
 	it('return correcct game ID', function() {
@@ -19,28 +22,59 @@ describe('Kancolle game', function() {
 		assert.equal(kancolle._getAppId(), ID, 'Kancolle app id should be ' + ID);
 	})
 
-	it('is NOT on maintenance', function(done) {
-		var sourceText = 
+	describe('Maintenance test', function() {
+
+		var code;
+		beforeEach(function() {
+		code = 
 		`
 		var ConstServerInfo = {servFoo:"servBar"}, ConstURLInfo = {urlFoo:"urlBar"};
 		var MaintenanceInfo = {};
-		MaintenanceInfo.IsDoing       = 0;
-		MaintenanceInfo.IsEmergency   = 0;
+		MaintenanceInfo.IsDoing       = %d;
+		MaintenanceInfo.IsEmergency   = %d;
 		MaintenanceInfo.StartDateTime = Date.parse("2016/07/15 11:00:00");
 		MaintenanceInfo.EndDateTime   = Date.parse("2016/07/15 17:50:00");
 		`
-
-		var httpRequest = sinon.stub(rp, 'get').returns({
-			then: function(bodyCallback) {bodyCallback(sourceText); return this;},
-			catch: function() {}
 		})
 
-		kancolle.isOnMaintenance(function(error, maintenanceInfo) {
-			assert.isNull(error);
-			assert.isFalse(maintenanceInfo.isMaintain);
+		it('is NOT on maintenance', function(done) {
+			var sourceText = sprintf(code, 0, 0);
+			var httpRequest = sinon.stub(rp, 'get').returns({
+				then: function(bodyCallback) {bodyCallback(sourceText); return this;},
+				catch: function() {}
+			})
 
-			httpRequest.restore();
-			done();
+			kancolle.isOnMaintenance(function(error, maintenanceInfo) {
+				assert.isNull(error);
+				assert.isFalse(maintenanceInfo.isMaintain);
+
+				httpRequest.restore();
+				done();
+			})
 		})
+
+		async.forEach([
+		{doing: 0, emergency: 1},
+		{doing: 1, emergency: 0},
+		{doing: 1, emergency: 1}], function(mode, callback) {
+			it(sprintf('is on maintenance (doing = %d, emergency = %d)', mode.doing, mode.emergency), function(done) {
+				var sourceText = sprintf(code, mode.doing, mode.emergency);
+				var httpRequest = sinon.stub(rp, 'get').returns({
+					then: function(bodyCallback) {bodyCallback(sourceText); return this;},
+					catch: function() {}
+				})
+
+				kancolle.isOnMaintenance(function(error, maintenanceInfo) {
+					assert.isNull(error);
+					assert.isTrue(maintenanceInfo.isMaintain);
+
+					httpRequest.restore();
+					done();
+				})
+			})
+
+			callback();
+		});
 	})
 })
+
