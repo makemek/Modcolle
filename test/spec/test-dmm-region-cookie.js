4d095bab0a8ad3f6fe86c113a86358c2e5cc4472
Makemek <makemek101@hotmail.com>
2016-07-22 20:02:23 +0700

    [injector] test cookie region revoke

--- test/spec/test-dmm-region-cookie.js
+++ test/spec/test-dmm-region-cookie.js
@@ -3,13 +3,15 @@
 const regionCookie = require('../../src/dmm/cookie/region-cookie');
 const Cookie = require('tough-cookie').Cookie;
 const sprintf = require('sprintf-js').sprintf
+const Injector = require('../../src/dmm/cookie/injector');
+const async = require('async');
 
 describe('Region cookie generator', function() {
 
-	[
+	async.forEach([
 	{key: 'ckcy', val: '1'}, 
 	{key: 'cklg', val: 'welcome'}
-	].forEach(function(expectedCookie) {
+	], function(expectedCookie) {
 		var expectName = expectedCookie.key;
 		var expectVal = expectedCookie.val;
 
@@ -33,4 +35,29 @@ describe('Region cookie generator', function() {
 		})
 	})
 
+	async.forEach([
+	[],
+	[new Cookie({key: 'ckcy', value: 1}).toString()],
+	[new Cookie({key: 'ckcy', value: 9999}).toString()]
+	], function(item) {
+		it('cookie ckcy is set to 1 with given cookie ' + item, function(done) {
+			var injector = new Injector(item);
+			injector.revokeRegionRestriction()
+			.end(function(error, cookies) {
+				assert.isNull(error, 'there should be no error');
+				var ckcy = cookies.filter(function(cookie) { return cookie.key == 'ckcy' });
+				var dmmDomainPath = ['/', '/netgame/', '/netgame_s/'];
+				assert.equal(ckcy.length, dmmDomainPath.length);
+
+				ckcy.forEach(function(cookie) {
+					assert.equal(cookie.key, 'ckcy');
+					assert.equal(cookie.value, 1);
+					assert.equal(cookie.domain, 'dmm.com');
+					assert.include(dmmDomainPath, cookie.path);
+				})
+				done();
+			});
+		})
+	})
+	
 })
