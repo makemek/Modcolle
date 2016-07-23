b2a2e2e6e5fe5d217cc78cb2a9e8aab9f3a6d9ae
Makemek <makemek101@hotmail.com>
2016-07-23 23:38:16 +0700

    [injector] log revokeRegionRestriction() and language()

--- src/dmm/cookie/injector.js
+++ src/dmm/cookie/injector.js
@@ -2,6 +2,7 @@
 
 const inherit = require('inherit');
 const async = require('async');
+const appLog = require('winston').loggers.get('app');
 const tough = require('tough-cookie');
 const Cookie = tough.Cookie;
 
@@ -15,10 +16,12 @@ var languagePreset = {
 var Injector = {
 
 	__constructor: function(subdomains, cookies) {
+		appLog.verbose('parse cookie');
 		if (cookies instanceof Array)
 		  cookies = cookies.map(Cookie.parse);
 		else
 		  cookies = [Cookie.parse(cookies)];
+		appLog.debug(cookies);
 
 		this.cookies = cookies;
 		this.tasks = [];
@@ -29,12 +32,17 @@ var Injector = {
 	revokeRegionRestriction: function() {
 		var targetCookie = {key: 'ckcy', value: 1};
 		var self = this;
+		appLog.verbose('push async task');
 		this.tasks.push(function(done) {
+			appLog.verbose('remove cookies that has value ' + targetCookie.key);
 			self.cookies = self.cookies.filter(function(cookie) {
 				return cookie.key != targetCookie.key;
 			})
+			appLog.debug(self.cookies);
 
+			appLog.verbose('merge generated cookies');
 			self.cookies = self.cookies.concat(generateCookies(targetCookie, [self.domain], self.subdomains));
+			appLog.debug(self.cookies);
 			done();
 		});
 
@@ -45,12 +53,17 @@ var Injector = {
 		language = language || languagePreset.japan;
 		var targetCookie = {key: 'cklg', value: language};
 		var self = this;
+		appLog.verbose('push async task');
 		this.tasks.push(function(done) {
+			appLog.verbose('remove cookies that has value ' + targetCookie.key);
 			self.cookies = self.cookies.filter(function(cookie) {
 				return cookie.key != targetCookie.key;
 			})
+			appLog.debug(self.cookies);
 
+			appLog.verbose('merge generated cookies');
 			self.cookies = self.cookies.concat(generateCookies(targetCookie, [self.domain], self.subdomains));
+			appLog.debug(self.cookies);
 			done();
 		});
 
@@ -58,6 +71,7 @@ var Injector = {
 	},
 
 	end: function(done) {
+		appLog.info("start queued cookie injector's tasks");
 		var self = this;
 		async.series(this.tasks, function(error) {
 			done(error, self.cookies);
@@ -66,6 +80,11 @@ var Injector = {
 }
 
 function generateCookies(keyVal, domains, paths) {
+	appLog.verbose('create cookies');
+	appLog.debug(keyVal);
+	appLog.debug(domains);
+	appLog.debug(paths);
+
 	var cookies = [];
 	domains.forEach(function(domain) {
 		paths.forEach(function(path) {
@@ -78,6 +97,7 @@ function generateCookies(keyVal, domains, paths) {
 		})
 	})
 
+	appLog.debug(cookies);
 	return cookies;
 }
 
