92ad07d46375b8c5519f43577bfabf33119257f2
Makemek <makemek101@hotmail.com>
2016-08-14 14:57:57 +0700

    move server configuration scripts to server.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = exports = require('./src/');
+module.exports = exports = require('./server');
--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
 'use strict';
 
-const nconf = require('nconf');
-const winston = require('winston');
-loadConfig();
-setupLogger();
-
-const App = require('./modcolle');
-
-function startServer(port) {
-	var modColle = new App();
-	modColle.start(port, function() {
-		winston.loggers.get('app').info('ModColle is ready!');
-	})
-}
-
-function loadConfig() {
-	nconf.argv().env();
-	nconf.add('settings', {type: 'file', file: 'src/config/settings.json'});
-	nconf.add('logger', {type: 'file', file: 'src/config/loggerSettings.json'});
-}
-
-function setupLogger() {
-	var loggerConfig = nconf.get('logger');
-	Object.keys(loggerConfig).forEach(function(key) {
-	    winston.loggers.add(key, loggerConfig[key]);
-	});
-}
-
-module.exports = exports = startServer(80);
+module.exports = exports = require('./modcolle');
