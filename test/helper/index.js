0ac0e148b05a3c99ec8a7c1696e9f7f5668f434c
Makemek <makemek101@hotmail.com>
2016-08-19 03:00:18 +0700

    configure sinon to disable fakeTimer and FakeServer
    To prevent cascading failure when one test throw an error

--- test/helper/index.js
+++ test/helper/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 require('../../src/');
+require('./sinon');
 require('./chai');
 require('./logger');
---
+++ test/helper/sinon.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var sinon = require('sinon');
+sinon.config = {
+	useFakeTimers: false,
+	useFakeServer: false
+};
