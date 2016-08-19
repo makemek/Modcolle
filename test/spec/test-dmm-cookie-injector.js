6bb681506f9515be4d159d7047c1312755e57b63
Makemek <makemek101@hotmail.com>
2016-08-20 02:20:29 +0700

    [cookie injector] more descriptive testcase

--- test/spec/test-dmm-cookie-injector.js
+++ test/spec/test-dmm-cookie-injector.js
@@ -8,13 +8,13 @@ const async = require('async');
 describe('Region cookie generator', function() {
 
 	async.forEach([
-	[],
-	[new Cookie({key: 'ckcy', value: 1}).toString()],
-	[new Cookie({key: 'ckcy', value: 9999}).toString()]
+	{case: 'no cookie', input: []},
+	{case: 'valid pre-existing cookie', input: [new Cookie({key: 'ckcy', value: 1}).toString()]},
+	{case: 'invalid pre-existing cookie', input: [new Cookie({key: 'ckcy', value: 9999}).toString()]}
 	], function(item) {
-		it('cookie ckcy is set to 1 with given cookie ' + item, function() {
+		it('should revoke region restriction with ' + item.case, function() {
 			var dmmDomainPath = ['/', '/netgame/', '/netgame_s/'];
-			var injector = new Injector(dmmDomainPath, item);
+			var injector = new Injector(dmmDomainPath, item.input);
 			var cookies = injector.revokeRegionRestriction()
 
 			var ckcy = cookies.filter(function(cookie) { return cookie.key == 'ckcy' });
@@ -29,18 +29,21 @@ describe('Region cookie generator', function() {
 		})
 	})
 
-	async.forEach(['ja', 'en'], function(lang) {
-		it('set language cookie to ' + lang, function() {
+	async.forEach([
+		{case: 'Japanese', input: 'ja'},
+		{case: 'English', input: 'en'}
+		], function(lang) {
+		it('set language cookie to ' + lang.case, function() {
 			var dmmDomainPath = ['/', '/netgame/', '/netgame_s/'];
-			var injector = new Injector(dmmDomainPath, [new Cookie({key: 'cklg', value: lang}).toString()]);
-			var cookies = injector.language(lang);
+			var injector = new Injector(dmmDomainPath, [new Cookie({key: 'cklg', value: lang.input}).toString()]);
+			var cookies = injector.language(lang.input);
 
 			var cklg = cookies.filter(function(cookie) { return cookie.key == 'cklg' });
 			assert.equal(cklg.length, dmmDomainPath.length);
 
 			cklg.forEach(function(cookie) {
 				assert.equal(cookie.key, 'cklg');
-				assert.equal(cookie.value, lang);
+				assert.equal(cookie.value, lang.input);
 				assert.equal(cookie.domain, 'dmm.com');
 				assert.include(dmmDomainPath, cookie.path);
 			})
