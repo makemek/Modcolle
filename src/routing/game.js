5161e75955d0b3b333cdc9ff6e531a49bb80314e
Makemek <makemek101@hotmail.com>
2016-08-23 00:55:24 +0700

    not authenticated -> render index page and move kancolle game page to kancolle.hbs

--- src/routing/game.js
+++ src/routing/game.js
@@ -2,36 +2,17 @@
 
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const API_TOKEN = process.env.API_TOKEN;
-const moment = require('moment-timezone');
 const passport = require('passport');
 const dmmAuthenticator = require('../middleware/dmm-passport');
 
-router.get('/', function (req, res) {
-	res.render('index', {
-		API_TOKEN: API_TOKEN,
-		API_START_TIME: currentJapanUnixTime()
-	})
+router.get('/', function (req, res, next) {
+	if(!req.isAuthenticated())
+		return res.render('index');
 });
 
 router.post('/login', passport.authenticate('local', {
-	successRedirect: '/kancolle',
+	successRedirect: '/',
 	failureRedirect: '/'
 }))
-
-router.get('/kancolle', dmmAuthenticator.isAuthenticated, function(req, res) {
-	res.send(req.user);
-})
-
-function currentJapanUnixTime() {
-	var timezone = 'Japan';
-
-	var today = new Date();
-	var todayStamp = today.toISOString();
-	var datetimeMs = todayStamp.replace(/-|:|Z/g, '');
-
-	return moment.tz(datetimeMs, timezone).format('x');
-}
 
 module.exports = exports = router;
