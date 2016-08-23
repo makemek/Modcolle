a68f2408e6e62f1fc1a1e6676233ff39eb7e23b4
Makemek <makemek101@hotmail.com>
2016-08-24 01:00:26 +0700

    response kancolle launch url after logged in

--- src/routing/game.js
+++ src/routing/game.js
@@ -4,10 +4,22 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const dmmAuthenticator = require('../middleware/dmm-passport');
+const osapi = require('../dmm/osapi');
+const kancolle = require('../kancolle/')
 
 router.get('/', function (req, res, next) {
 	if(!req.isAuthenticated())
 		return res.render('index');
+
+	osapi.getGameInfo(kancolle.appId, req.user, function(error, gadgetInfo) {
+		if(error)
+			return next(error);
+		kancolle.launch(gadgetInfo)
+		.then(function(url) {
+			res.render('kancolle', {flashUrl: url});
+		})
+		.catch(next);
+	})
 });
 
 router.post('/login', passport.authenticate('local', {
