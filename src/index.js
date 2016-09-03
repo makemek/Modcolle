f0fd78830c2bf121277e89df0e61578bcffd6295
Makemek <makemek101@hotmail.com>
2016-09-04 00:10:30 +0700

    turn off logging by default

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 process.env.KANCOLLE_BASE_DIR = process.env.KANCOLLE_BASE_DIR || 'kancolle';
 process.env.SESSION_SECRET = process.env.SESSION_SECRET || 'DEFAULT';
 process.env.LOGGER_LEVEL = process.env.LOGGER_LEVEL || 'info';
-process.env.LOGGER_SILENT = (process.env.LOGGER_SILENT || 'false').toLowerCase().trim();
+process.env.LOGGER_SILENT = (process.env.LOGGER_SILENT || 'true').toLowerCase().trim();
 process.env.LOGGER_TIMESTAMP = (process.env.LOGGER_TIMESTAMP || 'false').toLowerCase().trim();
 process.env.LOGGER_PRETTY_PRINT = (process.env.LOGGER_PRETTY_PRINT || 'false').toLowerCase().trim();
 
