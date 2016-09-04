fba14c94dd74b97d64d2289e0e3e7ffb7a122194
Makemek <makemek101@hotmail.com>
2016-09-04 18:44:16 +0700

    [kancolle server] get a server by its hostname and by world id

--- src/kancolle/server/index.js
+++ src/kancolle/server/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const KcServer = require('./server');
+const urlparse = require('url-parse');
 const ACCESS_KEY = 'KANCOLLE_SERVER_';
 var servers = {};
 
 for(let worldId = 1; process.env.hasOwnProperty(ACCESS_KEY + worldId); ++worldId) {
-	let host = process.env[ACCESS_KEY + worldId];
-	servers[worldId] = new KcServer(worldId, host);
+	let url = urlparse(process.env[ACCESS_KEY + worldId]);
+	let kancolleServer = new KcServer(worldId, url.href);
+	servers[worldId] = kancolleServer;
+	servers[url.hostname] = kancolleServer;
 }
 
 module.exports = exports = servers;
