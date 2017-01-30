38ba7bdb48208bf9ef86de44088597e2965045dd
Makemek <makemek101@hotmail.com>
2017-01-31 01:52:40 +0700

    automatically sync .css and .hbs to browser when modified

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,12 @@ const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const cleanCSS = require('gulp-clean-css')
 const rename = require('gulp-rename')
+const bs = require('browser-sync').create()
+const nodemon = require('gulp-nodemon')
 
+const devPort = 8080
 const css = 'src/views/*.css'
+const htmlTemplate = 'src/views/**/*.hbs'
 
 gulp.task('build', ['build:css'])
 
@@ -16,8 +20,30 @@ gulp.task('build:css', () => {
   .pipe(postcss([autoprefixer()]))
   .pipe(cleanCSS())
   .pipe(gulp.dest('src/views/public/css'))
+  .pipe(bs.stream())
 })
 
-gulp.task('watch', () => {
+gulp.task('nodemon', done => {
+  let start = false
+  return nodemon({
+    script: 'bin/www',
+    ignore: ['gulpfile.js', 'node_modules/', 'src/views/'],
+    env: {PORT: devPort}
+  })
+  .on('start', () => {
+    if(!start) {
+      start = true
+      done()
+    }
+  })
+})
+
+gulp.task('browser-sync', ['nodemon'], () => {
+  bs.init({
+    proxy: `localhost:${devPort}`,
+    open: false
+  })
+
   gulp.watch(css, ['build:css'])
+  gulp.watch(htmlTemplate, bs.reload)
 })
