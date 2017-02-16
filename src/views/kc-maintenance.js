85bee4a6433f74feabad48d8c4ba2fdcea7742c6
Makemek <makemek101@hotmail.com>
2017-02-17 02:42:18 +0700

    setup browser-side javascript to be compatible with modcolle linting rules
    * use babel to deal with browser js compatibility when build js
    * lint kc-maintainance to comply with linting rules
    * ignore file type *.min.js from linting

--- src/views/kc-maintenance.js
+++ src/views/kc-maintenance.js
@@ -1,16 +1,23 @@
-var timer_day = document.getElementById('day')
-var timer_hour = document.getElementById('hour')
-var timer_minute = document.getElementById('minute')
-var timer_second = document.getElementById('second')
-var maintenanceNotice = document.getElementById('maintenance-timer')
+/* Depends on http://203.104.209.7/gadget/js/kcs_const.js */
+/* global MaintenanceInfo */
+
+'use strict'
+
+const timer = {
+  day: document.getElementById('day'),
+  hour: document.getElementById('hour'),
+  minute: document.getElementById('minute'),
+  second: document.getElementById('second')
+}
+const maintenanceNotice = document.getElementById('maintenance-timer')
 
 maintenanceCheck()
 
 function maintenanceCheck() {
-  var now = Date.now()
-  var onMaintenance = now > MaintenanceInfo.StartDateTime && now < MaintenanceInfo.EndDateTime
+  const now = Date.now()
+  const onMaintenance = now > MaintenanceInfo.StartDateTime && now < MaintenanceInfo.EndDateTime
   if(onMaintenance) {
-    var remaining = MaintenanceInfo.EndDateTime - now
+    const remaining = MaintenanceInfo.EndDateTime - now
     maintenanceNotice.style.display = 'block'
     tick(remaining)
     setTimeout(maintenanceCheck, 1000)
@@ -20,13 +27,8 @@ function maintenanceCheck() {
 }
 
 function tick(timeRemaining) {
-  var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24))
-  var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60))
-  var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000)
-
-  timer_day.innerHTML = days
-  timer_hour.innerHTML = hours
-  timer_minute.innerHTML = minutes
-  timer_second.innerHTML = seconds
+  timer.day.innerHTML = Math.floor(timeRemaining / (1000 * 60 * 60 * 24))
+  timer.hour.innerHTML = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  timer.minute.innerHTML = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60))
+  timer.second.innerHTML = Math.floor((timeRemaining % (1000 * 60)) / 1000)
 }
