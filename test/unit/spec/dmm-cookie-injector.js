087d1d75eb61631aba23853756c3ee8db5157750
Apipol Niyomsak <makemek101@hotmail.com>
2017-04-26 14:03:38 +0700

    src: remove injecting 'cklg' cookie
    Injecting DMM cookie 'ckcy' is sufficient enough to access DMM game.
    Injecting 'cklg' to DMM cookie is not neccessary.
    
    PR-URL: https://github.com/makemek/modcolle/pull/37

--- test/unit/spec/dmm-cookie-injector.js
+++ test/unit/spec/dmm-cookie-injector.js
@@ -32,28 +32,6 @@ describe('Region cookie generator', () => {
     })
   })
 
-  async.forEach([
-    {case: 'Japanese', input: 'ja'},
-    {case: 'English', input: 'en'}
-  ], lang => {
-    it('set language cookie to ' + lang.case, () => {
-      const injector = new Injector([new Cookie({key: 'cklg', value: lang.input}).toString()], dmmDomainPath)
-      const cookies = injector.language(lang.input)
-
-      const cklg = cookies.filter(cookie => {
-        return cookie.key == 'cklg'
-      })
-      cklg.length.should.equal(dmmDomainPath.length)
-
-      cklg.forEach(cookie => {
-        cookie.key.should.equal('cklg')
-        cookie.value.should.equal(lang.input)
-        cookie.domain.should.equal('dmm.com')
-        dmmDomainPath.should.containEql(cookie.path)
-      })
-    })
-  })
-
   it('should set default cookie to empty array', () => {
     const injector = new Injector()
     injector.cookies.should.deepEqual([], 'should be empty array')
@@ -62,14 +40,5 @@ describe('Region cookie generator', () => {
   it('should set default sub domain to root page', () => {
     const injector = new Injector()
     injector.subdomains.should.containEql('/')
-  })
-
-  it('should set default language to Japanese', () => {
-    const injector = new Injector()
-    const cookies = injector.language()
-
-    cookies.forEach(cookie => {
-      cookie.value.should.equal(Injector.language.japan)
-    })
   })
 })
