5f79b2c787662881c11cb8bb4bb7fe8680e27d23
Apipol Niyomsak <makemek101@hotmail.com>
2017-06-19 22:59:36 +0700

    separate view and api

--- src/modcolle.js
+++ src/modcolle.js
@@ -2,21 +2,17 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const expressHandlebars = require('express-handlebars')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const loginStrategy = require('./login-strategy')
 const app = express()
 const log = require('./logger')('app:router')
 const router = require('./routing/')
-const path = require('path')
 
 log.info('=== Welcome to Modcolle ===')
 setupMiddleware()
-setupTemplateEngine()
 setupDefaultLocalResponseHeader()
 setupRouting()
-setupStaticResourceDirectory()
 log.info('=== Finished App Configuration ===')
 
 function setupDefaultLocalResponseHeader() {
@@ -43,31 +39,6 @@ function setupMiddleware() {
   passport.use('dmm-session', new LocalStrategy({
     usernameField: 'dmm_session', passwordField: 'dmm_session'},
   loginStrategy.dmmSession))
-}
-
-function setupTemplateEngine() {
-  const engineName = 'hbs'
-  const templateExtension = 'hbs'
-  const baseDirView = 'src/views'
-
-  const options = {
-    extname: templateExtension,
-
-    layoutsDir: baseDirView + '/layouts',
-    partialsDir: baseDirView + '/partials'
-  }
-  log.info(`setup template engine ${engineName}`, options)
-  const hbs = expressHandlebars.create(options)
-
-  app.engine(engineName, hbs.engine)
-  app.set('views', baseDirView)
-  app.set('view engine', engineName)
-}
-
-function setupStaticResourceDirectory() {
-  const staticDir = path.join(__dirname, 'views/public')
-  log.info('setup static directory', staticDir)
-  app.use('/', express.static(staticDir))
 }
 
 module.exports = app
