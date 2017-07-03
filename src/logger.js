9420826d3b6d77c7334aa80ba765b1860de562d0
Apipol Niyomsak <makemek101@hotmail.com>
2017-07-03 22:59:55 +0700

    src: fix listener memory leak warnings
    Due to pino logger are instantiate incorrectly

--- src/logger.js
+++ src/logger.js
@@ -3,16 +3,15 @@
 const LOGGER_LEVEL = process.env.LOGGER_LEVEL
 const LOGGER_PRETTY = process.env.LOGGER_PRETTY
 
-const pino = require('pino')
+const pino = require('pino')({
+  level: LOGGER_LEVEL,
+  prettyPrint: LOGGER_PRETTY === 'true'
+})
 const labels = ['app:middleware', 'app:router', 'service:dmm', 'service:kancolle']
 
 const loggers = {}
 labels.forEach(label => {
-  loggers[label] = pino({
-    name: label,
-    level: LOGGER_LEVEL,
-    prettyPrint: LOGGER_PRETTY === 'true'
-  })
+  loggers[label] = pino.child({label})
 })
 
 module.exports = function(label) {
